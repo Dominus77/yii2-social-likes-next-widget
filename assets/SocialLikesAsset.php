886154c60ecaf9c9683cd37ab3a636ebaba538fc
Alexey Shevchenko <ivanovosity@gmail.com>
2017-10-25 17:49:26 +0300

    Refactoring

--- assets/SocialLikesAsset.php
+++ assets/SocialLikesAsset.php
@@ -10,15 +10,10 @@ use yii\web\AssetBundle;
  */
 class SocialLikesAsset extends AssetBundle
 {
-    public $sourcePath = '@dominus77/sociallikesnext/assets/src/dist';
-
-    public $css = [];
-
-    public $js = [];
-
     public function init()
     {
         $min = YII_ENV_DEV ? '' : '.min';
+        $this->sourcePath = __DIR__ . '/src/dist';
         $this->css[] = 'social-likes.css';
         $this->js[] = 'social-likes' . $min . '.js';
     }
